5d1d3029acf00386044bf517a7708ab940d2b48f
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2023-12-01 19:12:57 +0600

    Add files via upload

---
+++ patternQ1.java
@@ -0,0 +1,13 @@
+public class patternQ1 {
+    public static void main (String[]args){
+        /*
+         * print the pattern 
+         * 
+         **
+         ***
+         ****
+         by using only output statement...
+         */
+        System.out.print("*\n**\n***\n****");
+    }
+}
